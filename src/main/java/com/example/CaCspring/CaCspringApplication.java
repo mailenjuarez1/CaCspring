95e957581d810441b480dfd7c8b37f0495d46d2f
mailenjuarez1 <93502464+mailenjuarez1@users.noreply.github.com>
2023-07-16 20:08:40 -0300

    first commit

---
+++ src/main/java/com/example/CaCspring/CaCspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.CaCspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaCspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaCspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CaCspring/CaCspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CaCspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaCspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
